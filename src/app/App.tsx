7a511130ccdf60b5e305af482cd3544f1aad399d
alexlee-dev <alex@alexlee.dev>
2021-01-19 22:27:37 -0800

    🚀 Hello World! TypeScript + Three.js + React

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import LivingRoom from "./LivingRoom";
+
+const App: React.FunctionComponent<unknown> = () => {
+  return <LivingRoom />;
+};
+
+export default App;
